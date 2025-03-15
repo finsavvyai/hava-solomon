5c696a678e63b6fd795d26f46681e19db0ff08e2
Shachar Solomon <Shahar.solomon@teliacompany.com>
2025-03-16 00:37:20 +0200

    Welcome alignment

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import staticPlugin from 'vite-plugin-static';
-
 
 export default defineConfig({
-  plugins: [react(),staticPlugin()],
-  base: '/hava-solomon', // Change to match your GitHub repository name
+  plugins: [react()],
+    base: "/hava-solomon/",
   server: {
     port: 5174,
     host: '0.0.0.0',
